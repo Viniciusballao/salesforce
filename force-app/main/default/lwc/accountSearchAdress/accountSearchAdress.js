afbbf23400df1eaa2be6fef30f415e33a5872d10
Vinicius Ballao <viniciuss.lima@wisereducacao.com>
2023-05-27 19:54:40 -0300

    Pausing to resolve technical test

--- force-app/main/default/lwc/accountSearchAdress/accountSearchAdress.js
+++ force-app/main/default/lwc/accountSearchAdress/accountSearchAdress.js
@@ -1,6 +1,35 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track} from 'lwc';
+import { CloseActionScreenEvent } from 'lightning/actions';
 
 import searchZipCode from '@salesforce/apex/AccountSearchAddressController.fetchAddress';
 import updateAccount from '@salesforce/apex/AccountSearchAddressController.saveAccount';
 
-export default class AccountSearchAddress extends LightningElement {}
+export default class AccountSearchAddress extends LightningElement {
+    @track cep;
+    @track logradouro;
+    @track bairro;
+    @track cidade;
+    @track uf;
+
+    handleInputChange(event) {
+        this.cep = event.detail.value.replace(/\D/g,"");
+        console.log(this.cep.length);
+        if(this.cep.replace(/\D/g,"").length == 8){
+            console.log('TAMO JUNTO TIME');
+            searchZipCode({cep:this.cep}).then(response => {
+                console.log(response);
+                console.log('New');
+
+                this.bairro = response.bairro;
+                this.logradouro = response.logradouro;
+                this.cidade = response.localidade;
+                this.uf = response.uf;                
+            }).catch(error => {
+                console.log('Error' + error.body.message);
+            })
+        }else{
+            this.logradouro = '';
+        }
+    }
+
+}
